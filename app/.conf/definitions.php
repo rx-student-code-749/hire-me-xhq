3145d7955baa9f65c088e61445d255d958ed984d
R <rapheekwon@gmail.com>
2018-11-19 21:20:08 -0500

    App configuration files.

---
+++ app/.conf/definitions.php
@@ -0,0 +1,6 @@
+<?php
+
+define ('CONFIG_DIR__TRAILING_SLASH', __DIR__ . "/");
+define ('APPLICATION_DIR__TRAILING_SLASH', __DIR__ . "/../.app/");
+define ('CONFIG_DIR__NO_TRAILING_SLASH', __DIR__);
+define ('APPLICATION_DIR__NO_TRAILING_SLASH', __DIR__ . "/../.app");
